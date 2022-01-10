18042513defacaef8127341b72fd8c745cc3cf85
Ahmad Febrianto <achmadfebryanto@gmail.com>
2022-01-10 14:36:01 +0700

    Add toast when dice is rolled

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.diceroller
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,13 +15,17 @@ class MainActivity : AppCompatActivity() {
         rollDice()
 
         val btRollDice: Button = findViewById(R.id.bt_roll)
-        btRollDice.setOnClickListener { rollDice() }
+        btRollDice.setOnClickListener {
+            val rolledNumber = rollDice()
+            showToast("Rolled number: $rolledNumber")
+        }
     }
 
-    private fun rollDice() {
+    private fun rollDice(): Int {
         val diceImage: ImageView = findViewById(R.id.iv_dice)
         val dice = Dice(6)
-        val diceImageResource = when (dice.roll()) {
+        val rolledNUmber = dice.roll()
+        val diceImageResource = when (rolledNUmber) {
             1 -> R.drawable.dice_1
             2 -> R.drawable.dice_2
             3 -> R.drawable.dice_3
@@ -28,5 +34,14 @@ class MainActivity : AppCompatActivity() {
             else -> R.drawable.dice_6
         }
         diceImage.setImageResource(diceImageResource)
+        diceImage.contentDescription = rolledNUmber.toString()
+
+        return rolledNUmber
+    }
+
+    private fun showToast(message: String) {
+        val toast = Toast.makeText(this, message, Toast.LENGTH_SHORT)
+        toast.setGravity(Gravity.TOP, 0, 200)
+        toast.show()
     }
 }
